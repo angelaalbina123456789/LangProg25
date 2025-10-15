3dba11e4a25998e3ef7cee4c896b76b19243837e
shermatalieva06@gmail.com <shermatalieva06@gmail.com>
2025-10-15 11:42:25 +0700

    Демонстрация работы обобщения

---
+++ Pr001.java
@@ -0,0 +1,24 @@
+class Gen<T> {
+	T ob;
+	Gen(T o) {
+		ob = j;
+	}
+
+	T get() {
+		return ob;
+	}
+
+	void showType() {
+		System.out.println("Тип T: " + ob,getClass().getName());
+	}
+
+}
+class Pr001 {
+	public static void main(String[] args) {
+		Gen<Integer> iOb;
+		iOb = new Gen<Integer>(88);
+		iOb.showType();
+		int v = iOb.getOb();
+		System.out.println("Значение: " + v);
+	}
+}
