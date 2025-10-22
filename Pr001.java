7ddb00082174d310c4576234f139f4a481f35353
shermatalieva06@gmail.com <shermatalieva06@gmail.com>
2025-10-22 09:59:14 +0700

    добавили работу со строковым объектом в обобщеном классе

--- Pr001.java
+++ Pr001.java
@@ -1,24 +1,40 @@
 class Gen<T> {
+	//объявление переменной обобщенного типа
 	T ob;
+	//определение конструктора, в качестве параметра указываем объект обобщенного типа
 	Gen(T o) {
-		ob = j;
+		ob = o;
 	}
+	//метод возвращающий объект
 
-	T get() {
+	T getOb() {
 		return ob;
 	}
-
+	//метод получающий информацию об имени типа
 	void showType() {
-		System.out.println("Тип T: " + ob,getClass().getName());
+		System.out.println("Тип T: " + ob.getClass().getName());
 	}
 
 }
 class Pr001 {
 	public static void main(String[] args) {
+		//создаем  ссылочную переменную на объект типа Integer
 		Gen<Integer> iOb;
+		//присваиваем ссылку на новый объект
 		iOb = new Gen<Integer>(88);
+		//выводим информацию о типе объекта
 		iOb.showType();
+		//присваиваем целой переменной значение, хранящееся в объекте
 		int v = iOb.getOb();
 		System.out.println("Значение: " + v);
+
+		System.out.println();
+		//создаем ссылочную переменную на объект типа String и присваиваем ссылку на новый объект 
+		Gen<String> strOb = new Gen<String>("Строка обобщеного класса");
+		//выводим информацию о типе объекта
+		strOb.showType();
+		//присваиваем строковые переменные значения хранящееся в объекте
+		String str = strOb.getOb();
+		System.out.println("Значение в переменной str: " + str);
 	}
 }
